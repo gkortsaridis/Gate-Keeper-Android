7f34d56f7114023eb629605d0022b1cf35aa4354
George Kortsaridis <gkortsaridis@gmail.com>
2019-11-20 11:05:59 +0000

    Retrieving Logins by Account ID

--- app/src/main/java/gr/gkortsaridis/gatekeeper/Login.kt
+++ app/src/main/java/gr/gkortsaridis/gatekeeper/Login.kt
@@ -1,19 +1,30 @@
 package gr.gkortsaridis.gatekeeper
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.QueryDocumentSnapshot
+
 class Login {
 
+    var id : String
+    var account_id: String
     var name: String
     var username: String
     var password: String
     var url: String
     var notes: String
+    var date_created: Timestamp
+    var date_modified: Timestamp
 
-    constructor(name: String, username: String, password: String, url: String, notes: String) {
-        this.name = name
-        this.username = username
-        this.password = password
-        this.url = url
-        this.notes = notes
+    constructor(firestoreSnapShot: QueryDocumentSnapshot) {
+        this.id = firestoreSnapShot.id
+        this.account_id = firestoreSnapShot["account_id"] as String
+        this.name = firestoreSnapShot["name"] as String
+        this.username = firestoreSnapShot["username"] as String
+        this.password = firestoreSnapShot["password"] as String
+        this.url = firestoreSnapShot["url"] as String
+        this.notes = firestoreSnapShot["notes"] as String
+        this.date_created = firestoreSnapShot["date_added"] as Timestamp
+        this.date_modified = firestoreSnapShot["date_modified"] as Timestamp
     }
 
     override fun toString(): String {
--- app/src/main/java/gr/gkortsaridis/gatekeeper/LoginsFragment.kt
+++ app/src/main/java/gr/gkortsaridis/gatekeeper/LoginsFragment.kt
@@ -2,49 +2,55 @@ package gr.gkortsaridis.gatekeeper
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginsFragment : Fragment() {
 
+    private var TAG = "_LOGINS_FRAGMENT_"
+
     private lateinit var loginsRV: RecyclerView
+    private lateinit var db: FirebaseFirestore
+
+    override fun onStart() {
+        super.onStart()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_logins, container, false)
-
         loginsRV = view.findViewById(R.id.logins_recycler_view)
 
-        val logins = ArrayList<Login>()
-        logins.add(Login("Personal Google", "gkortsaridis@gmail.com", "password1", "url1", "note1"))
-        logins.add(Login("Twitter", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("AppleID", "gkortsaridis@gmail.com", "password1", "url1", "note1"))
-        logins.add(Login("Facebook", "gkortsaridis@gmail.com", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
-        logins.add(Login("Instagram", "gkortsaridis", "password1", "url1", "note1"))
+        retrieveLoginsByAccountID()
 
+        val logins = ArrayList<Login>()
         loginsRV.layoutManager = LinearLayoutManager(activity)
         loginsRV.adapter = LoginsRecyclerViewAdapter(activity!!.baseContext, logins)
 
         return view
     }
 
+    private fun retrieveLoginsByAccountID() {
+        val accountID = GateKeeperApplication.userAccount.id
+
+        Log.i(TAG, "Retrieving Logins for Account : $accountID")
+
+        db = FirebaseFirestore.getInstance()
+        db.collection("logins")
+            .whereEqualTo("account_id",accountID)
+            .get().addOnSuccessListener { result ->
+            for (document in result) {
+                val login = Login(document)
+                Log.i(TAG, login.toString())
+            }
+        }
+    }
 
 }
