c67dc0c4c55d711170a9854e7b2a30940cd1598a
George Kortsaridis <gkortsaridis@gmail.com>
2019-12-05 15:56:04 +0000

    Refactored Vault Selector Activity

--- app/src/main/java/gr/gkortsaridis/gatekeeper/UI/SelectVaultActivity.kt
+++ app/src/main/java/gr/gkortsaridis/gatekeeper/UI/SelectVaultActivity.kt
@@ -3,6 +3,7 @@ package gr.gkortsaridis.gatekeeper.UI
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import gr.gkortsaridis.gatekeeper.Entities.Vault
@@ -14,10 +15,19 @@ import gr.gkortsaridis.gatekeeper.UI.RecyclerViewAdapters.VaultSelectRecyclerVie
 class SelectVaultActivity : AppCompatActivity(), VaultClickListener {
 
     private lateinit var vaultsRecyclerView: RecyclerView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_vault)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+        supportActionBar?.title = "Select Vault"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
 
         vaultsRecyclerView = findViewById(R.id.vault_recycler_view)
         vaultsRecyclerView.layoutManager = LinearLayoutManager(this)
