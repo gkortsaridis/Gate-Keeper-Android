c3af60ed2716543f956c8fd9fc2b8773275ae2a5
George Kortsaridis <george.kortsaridis@jet2.com>
2020-02-20 08:12:17 +0000

    Notes use vault colors

--- app/src/main/java/gr/gkortsaridis/gatekeeper/UI/Notes/NoteActivity.kt
+++ app/src/main/java/gr/gkortsaridis/gatekeeper/UI/Notes/NoteActivity.kt
@@ -14,10 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.Timestamp
-import gr.gkortsaridis.gatekeeper.Entities.Note
-import gr.gkortsaridis.gatekeeper.Entities.NoteColor
-import gr.gkortsaridis.gatekeeper.Entities.Vault
-import gr.gkortsaridis.gatekeeper.Entities.ViewDialog
+import gr.gkortsaridis.gatekeeper.Entities.*
 import gr.gkortsaridis.gatekeeper.GateKeeperApplication
 import gr.gkortsaridis.gatekeeper.Interfaces.NoteCreateListener
 import gr.gkortsaridis.gatekeeper.Interfaces.NoteDeleteListener
@@ -37,23 +34,12 @@ class NoteActivity : AppCompatActivity() {
     private lateinit var noteTitle: EditText
     private lateinit var noteBody : EditText
     private lateinit var noteModified: TextView
-    private lateinit var noteExtraBtn: ImageButton
-    private lateinit var bottomSheetLayout: LinearLayout
-    private lateinit var bottomSheetContainer: RelativeLayout
     private lateinit var noteBackground: RelativeLayout
     private lateinit var deleteNote: LinearLayout
-    private lateinit var circleBlue: LinearLayout
-    private lateinit var circleCream: LinearLayout
-    private lateinit var circleGreen: LinearLayout
-    private lateinit var circleOrange: LinearLayout
-    private lateinit var circlePink: LinearLayout
-    private lateinit var circleRed: LinearLayout
-    private lateinit var circleWhite: LinearLayout
-    private lateinit var circleYellow: LinearLayout
+    private lateinit var bottomView: LinearLayout
 
     private lateinit var noteColor: NoteColor
     private lateinit var note : Note
-    private lateinit var sheetBehavior : BottomSheetBehavior<LinearLayout>
     private var noteMenu : Int? = null
     private var isPinned : Boolean = false
     private lateinit var vaultView: LinearLayout
@@ -68,22 +54,12 @@ class NoteActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         deleteNote = findViewById(R.id.delete_note)
         noteTitle = findViewById(R.id.note_title)
-        noteExtraBtn = findViewById(R.id.note_extra)
-        bottomSheetLayout = findViewById(R.id.bottom_sheet)
         noteBackground = findViewById(R.id.note_background)
-        bottomSheetContainer = findViewById(R.id.bottom_container)
-        circleBlue = findViewById(R.id.circle_blue)
-        circleCream = findViewById(R.id.circle_cream)
-        circleGreen = findViewById(R.id.circle_green)
-        circleOrange = findViewById(R.id.circle_orange)
-        circlePink = findViewById(R.id.circle_pink)
-        circleRed = findViewById(R.id.circle_red)
-        circleWhite = findViewById(R.id.circle_white)
-        circleYellow = findViewById(R.id.circle_yellow)
         noteBody = findViewById(R.id.note_body)
         noteModified = findViewById(R.id.note_modified)
         vaultView = findViewById(R.id.vault_view)
         vaultName = findViewById(R.id.vault_name)
+        bottomView = findViewById(R.id.bottom_view)
 
         val noteId = intent.getStringExtra("note_id")!!
         if (noteId != "-1") {
@@ -116,17 +92,6 @@ class NoteActivity : AppCompatActivity() {
 
         noteTitle.setText(note.title)
         deleteNote.setOnClickListener { deleteNote() }
-        sheetBehavior = BottomSheetBehavior.from(bottomSheetLayout)
-        noteExtraBtn.setOnClickListener { sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED }
-
-        circleBlue.setOnClickListener { changeColor(NoteColor.Blue) }
-        circleCream.setOnClickListener { changeColor(NoteColor.Cream) }
-        circleGreen.setOnClickListener { changeColor(NoteColor.Green) }
-        circleOrange.setOnClickListener { changeColor(NoteColor.Orange) }
-        circlePink.setOnClickListener { changeColor(NoteColor.Pink) }
-        circleRed.setOnClickListener { changeColor(NoteColor.Red) }
-        circleWhite.setOnClickListener { changeColor(NoteColor.White) }
-        circleYellow.setOnClickListener { changeColor(NoteColor.Yellow) }
         vaultView.setOnClickListener { changeVault() }
 
         val formatter = SimpleDateFormat(GateKeeperConstants.dateOnlyFormat)
@@ -140,6 +105,23 @@ class NoteActivity : AppCompatActivity() {
 
     private fun updateUI() {
         vaultName.text = vaultToAdd.name
+        when (vaultToAdd.color) {
+            VaultColor.Red -> {
+                bottomView.setBackgroundResource(R.drawable.vault_color_red)
+            }
+            VaultColor.Green -> {
+                bottomView.setBackgroundResource(R.drawable.vault_color_green)
+            }
+            VaultColor.Blue -> {
+                bottomView.setBackgroundResource(R.drawable.vault_color_blue)
+            }
+            VaultColor.Yellow -> {
+                bottomView.setBackgroundResource(R.drawable.vault_color_yellow)
+            }
+            VaultColor.White -> {
+                bottomView.setBackgroundResource(R.drawable.vault_color_white)
+            }
+        }
     }
 
     private fun changeVault() {
@@ -150,9 +132,9 @@ class NoteActivity : AppCompatActivity() {
     }
 
     private fun changeColor(color: NoteColor) {
-        noteColor = color
-        noteBackground.setBackgroundColor(Color.parseColor(color.value))
-        bottomSheetLayout.setBackgroundColor(Color.parseColor(color.value))
+        //noteColor = color
+        //noteBackground.setBackgroundColor(Color.parseColor(color.value))
+        //bottomSheetLayout.setBackgroundColor(Color.parseColor(color.value))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -180,21 +162,6 @@ class NoteActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         updateNoteAndFinish()
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        if (ev?.action === MotionEvent.ACTION_DOWN) {
-            if (sheetBehavior.state === BottomSheetBehavior.STATE_EXPANDED) {
-                val outRect = Rect()
-                bottomSheetLayout.getGlobalVisibleRect(outRect)
-
-                if (!outRect.contains(ev?.rawX.toInt(), ev.rawY.toInt())){
-                    sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-                }
-            }
-        }
-
-        return super.dispatchTouchEvent(ev)
     }
 
     private fun deleteNote() {
